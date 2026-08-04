8422004afcaec6131899c3bec87e5910cec5493b
unknown <bhumitsingh856@gmail.com>
2026-08-04 22:00:33 +0530

    Imported the USerSync in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
+import UserSync from "@/components/UserSync"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
